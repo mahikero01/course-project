d41285cfa77018dbaf4b0605d2d3a95eadbccdb1
mahikero <federicoparassarmiento@yahoo.com>
2016-06-21 16:31:10 -0400

    up

--- src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -1,15 +1,24 @@
 package com.infiniteskills.mvc.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.infiniteskills.mvc.data.entities.Project;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/home")
-	public String goHome() {
+
+	@RequestMapping("/")
+	public String goHome(Model model){
+		
+		Project project = new Project();
+		project.setName("First project");
+		project.setSponsor("nasa");
+		project.setDescription("thsi is a simple");
+		
+		model.addAttribute("currentProject", project);
 		return "home";
 	}
-	
 }
